tizenorg/platform.upstream.update-desktop-files

commit ca1b55134e1f144f7d06d69e9edb8197a3cfc9e1

Clean spec file for Yocto compliance
Also set a valid group tag.

Change-Id: Id3f3facdb07e216561ed8c2afda4e98227ba3b79
Signed-off-by: Kévin THIERRY <kevin.thierry@open.eurogiciel.org>

packaging/update-desktop-files.spec
@@ -3,13 +3,13 @@ Version:        12.1
 Release:        0
 Summary:        A Build Tool to Update Desktop Files
 License:        GPL-2.0+
-Group:          Development/Tools/Building
+Group:          Development/Tools
 BuildRoot:      %{_tmppath}/%{name}-%{version}-build
 Source:         tizen_update_desktop_file.sh
 Source1:        map-desktop-category.sh
 Source2:        macro
 Source4:        brp-trim-desktop.sh
-Source1001: 	update-desktop-files.manifest
+Source1001:     update-desktop-files.manifest
 # This is not true technically, but we do that to make the rpm macros from
 # desktop-file-utils available to most packages that ship a .desktop file
 # (since they already have a update-desktop-files BuildRequires).
@@ -36,21 +36,19 @@ cd %name
 %build
 
 %install
-mkdir -p $RPM_BUILD_ROOT%_rpmconfigdir
-install -m0755 %SOURCE0 %SOURCE1 $RPM_BUILD_ROOT%_rpmconfigdir
-install -m0644 -D %SOURCE2 $RPM_BUILD_ROOT/etc/rpm/macros.%name
-install -m0755 -D %SOURCE4 $RPM_BUILD_ROOT/usr/lib/rpm/brp-tizen.d/brp-70-trim-desktopfiles
+mkdir -p %{buildroot}%{_rpmconfigdir}
+install -m0755 %SOURCE0 %SOURCE1 %{buildroot}%{_rpmconfigdir}
+install -m0644 -D %SOURCE2 %{buildroot}%{_sysconfdir}/rpm/macros.%name
+install -m0755 -D %SOURCE4 %{buildroot}%{_libdir}/rpm/brp-tizen.d/brp-70-trim-desktopfiles
 
 %files
 %manifest %{name}.manifest
 %defattr(-,root,root)
-%_rpmconfigdir/*
+%{_rpmconfigdir}/*
 %exclude %_rpmconfigdir/brp-tizen.d
-/etc/rpm/*
+%{_sysconfdir}/rpm/*
 
 %files -n brp-trim-desktopfiles
 %manifest %{name}.manifest
 %defattr(-,root,root)
-%_rpmconfigdir/brp-tizen.d
-
-%changelog
+%{_rpmconfigdir}/brp-tizen.d

commit 41a39c9fe36dbbbc769ca391c32d5f8a3c0c0291

Fix x86_64 build: wrong macro
Directory "brp-tizen.d" is located in /usr/lib/rpm but was set to
%{_libdir}/rpm which expands to /usr/lib64/rpm for 64 bit architectures.
Using the macro %{_rpmconfigdir} solves this issue since it allways
expands to /usr/lib/rpm.

The issue was introduced by commit: ca1b55134e1f144f7d06d69e9edb8197a3cfc9e1

Change-Id: Ida24145c24913f782d1756130525c42ba71645e5
Signed-off-by: Kévin THIERRY <kevin.thierry@open.eurogiciel.org>

packaging/update-desktop-files.spec
@@ -39,7 +39,7 @@ cd %name
 mkdir -p %{buildroot}%{_rpmconfigdir}
 install -m0755 %SOURCE0 %SOURCE1 %{buildroot}%{_rpmconfigdir}
 install -m0644 -D %SOURCE2 %{buildroot}%{_sysconfdir}/rpm/macros.%name
-install -m0755 -D %SOURCE4 %{buildroot}%{_libdir}/rpm/brp-tizen.d/brp-70-trim-desktopfiles
+install -m0755 -D %SOURCE4 %{buildroot}%{_rpmconfigdir}/brp-tizen.d/brp-70-trim-desktopfiles
 
 %files
 %manifest %{name}.manifest